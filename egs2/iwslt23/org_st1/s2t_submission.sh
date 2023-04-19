f679760fbd09fe9f200e6d580c494f4e4615c802
ftshijt <ftshijt@gmail.com>
2023-04-18 20:28:40 -0500

    update param

--- egs2/iwslt23/org_st1/s2t_submission.sh
+++ egs2/iwslt23/org_st1/s2t_submission.sh
@@ -9,8 +9,8 @@ simuleval --standalone --remote-port 2023 \
 --st_train_config /espnet/egs2/iwslt23/org_st1/st_model_final/config.yaml \
 --st_model_file /espnet/egs2/iwslt23/org_st1/st_model_final/valid.acc.ave_10best_at300.pth \
 --disable_repetition_detection false \
---beam_size 5 \
---ctc_weight 0.3 \
+--beam_size 6 \
+--ctc_weight 0.1 \
 --sim_chunk_length 32000 \
 --backend streaming \
 --incremental_decode true \
